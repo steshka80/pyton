c77d43afdc7d0960b6a063df41f6c11ef8c5a0b8
steshka80 <106751602+steshka80@users.noreply.github.com>
2022-09-08 18:30:34 +0300

    Add files via upload

--- задача2.py
+++ задача2.py
@@ -1,1 +1,15 @@
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z 
+# для всех значений предикат
+
+flag = True
+for x in range(0, 2):
+    for y in range(0,2):
+        for z in range (0,2):
+            if ((not x) & (not y) & (not z)) != (not (x | y | z)):
+                flag=False
+                break
+if flag:
+    print ("Утверждение истинно при любых значения X Y Z")
+else:
+    print ("Утверждение не истинно")
 
